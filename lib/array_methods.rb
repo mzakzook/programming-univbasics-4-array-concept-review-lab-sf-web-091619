f7e4f131848185d51bcffcc5a608e98be8ce4706
sunny-utility-4827 <mzakzook@me.com>
2019-09-05 03:11:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   length = array.length
   length.times do |index|
     if array[index] == value_to_find
-      return i
+      return index
     end
   end
   return nil
